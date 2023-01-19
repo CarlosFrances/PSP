d5992a4f7b3b63b78eaca1a8ec2bfd2e072bd5a1
CarlosFrances <113194702+CarlosFrances@users.noreply.github.com>
2023-01-19 23:09:27 +0100

    u

--- Proyectos_clase/area_rectangulo.py
+++
@@ -1,9 +0,0 @@
-class Rectangulo:
-    def __init__(self,base,altura):
-        self.base=base
-        self.altura=altura
-    def area(self):
-        return self.base*self.altura
-
-rectangulo = Rectangulo(8,3)
-print(rectangulo.area())
--- Proyectos_clase/dia_uno.py
+++
@@ -1,5 +0,0 @@
-#Carlos Francés Sánchez - Dia Uno
-import math
-
-def divide_or_square(numero):
-    return math.sqrt(numero) if numero % 5 == 0 else numero % 5
