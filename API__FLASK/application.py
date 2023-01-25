5636ca66cfe273a6ffb4fb7f10b3c240f1a4e3fb
CarlosFrances <113194702+CarlosFrances@users.noreply.github.com>
2023-01-25 21:30:20 +0100

    u

--- API__FLASK/application.py
+++ API__FLASK/application.py
@@ -5,9 +5,11 @@
 # $env:FLASK_APP='application.py'
 
 from flask import Flask, request
+from flask_cors import CORS
 from flask import render_template
 
 application = Flask(__name__)
+CORS(application)
 
 #   PUERTO 5000
 @application.route("/") #RUTA
@@ -27,10 +29,36 @@ def divMiNombre():
 def return_username(username):
     return render_template("hello.html",username=username)  #RESPUESTA
 
-"""@application("/create-todo")
+@application.post("/create-todo")
 def create_todo():
+    data=request.data
+    data=data.decode()
+    print(data)
+    return "ok"
+
+@application.get("/get-todos")
+def get_todos():
+    data=[
+        {
+            "id":0,
+            "todo":"mi primer todo",
+            "completed":False
+        },
+        {
+            "id":1,
+            "todo":"mi segundo todo",
+            "completed":False
+        },
+        {
+            "id":2,
+            "todo":"mi tercer todo",
+            "completed":False
+        },
+    ]
+
+    return data
+
+@application.put("/complete-todo")
+def complete_todo():
     pass
 
-@application("/get-todo")
-def get_todo():
-    pass"""
