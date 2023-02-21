59330a33fa3f5b4512f165914a8e9a30348924a9
CarlosFrances <113194702+CarlosFrances@users.noreply.github.com>
2023-02-21 17:00:04 +0100

    u

--- prueba_examen/prueba_dict.py
+++ prueba_examen/prueba_dict.py
@@ -1,8 +1,21 @@
 from flask import Flask,jsonify
 
+
 app = Flask(__name__)
 
 input="01234567L;LuisGonzalez;luisgonzalez@mail.com;656343576;12.5\n71476342J;MacarenaRamirez;macarena@a.com;6943321;8"
+
+"""
+    request,Response,json,mysql.connector
+    credentials = {
+        "username":"admin",
+        "password":"",
+        "dbname":"database-1",
+        "host":"localhost.........etc"
+    }
+    @app.route(/getUsers)
+    def get_all_users(:
+"""
 
 def obtenerDiccionario(texto:str):
     diccionario = {}
